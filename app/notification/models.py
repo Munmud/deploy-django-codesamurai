4d9333a8a314b0c41f147a441c71fbaf2097e6ae
munmud <moontasir042@gmail.com>
2024-05-09 09:21:31 +0600

    feat: added dashboard table

--- app/notification/models.py
+++ app/notification/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 
 
 class Notification(models.Model):
@@ -7,3 +9,19 @@ class Notification(models.Model):
 
     def __str__(self):
         return self.message
+
+
+class NotificationBase(models.Model):
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True  # This model will not be created in the database
+
+
+class UserNotification(NotificationBase):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} Notification - {self.timestamp}"
--- app/notification/tasks.py
+++ app/notification/tasks.py
@@ -1,6 +1,35 @@
-from celery import shared_task
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from celery import shared_task
+
+from waste.models import STSManager, LandfillManager
+from .models import UserNotification
+
+
+@shared_task
+def send_notification_to_a_user(user, message):
+    notification = UserNotification(user=user, message=message)
+    notification.save()
+
+
+@shared_task
+def send_notification_to_group_user(group, message):
+    for user in group.users.all():
+        send_notification_to_a_user.delay(user, message)
+
+
+@shared_task
+def send_notification_to_sts_manager(sts, message):
+    for stsManagers in STSManager.objects.filter(sts=sts).all():
+        user = stsManagers.user
+        send_notification_to_a_user.delay(user, message)
+
+
+@shared_task
+def send_notification_to_landfill_manager(landfill, message):
+    for landfillManager in LandfillManager.objects.filter(landfill=landfill).all():
+        user = landfillManager.user
+        send_notification_to_a_user.delay(user, message)
 
 
 @shared_task
